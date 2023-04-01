019f6ffe0fdd4ff31ec9ee1449427ed3c5cdaeb6
unknown <marinalg705@gmail.com>
2023-04-01 15:31:49 +0300

    Исправлено

--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,8 @@ let popupOpenButton = profilElement.querySelector('.profile__button-pen');
 let popupSaveButton = popupElement.querySelector('.popup__save');
 let profilName = profilElement.querySelector('.profile__name');
 let profilJob = profilElement.querySelector('.profile__about');
-let nameInput = popupElement.querySelector('.popup__text_name');
-let jobInput = popupElement.querySelector('.popup__text_job');
+let nameInput = popupElement.querySelector('.popup__input_type_name');
+let jobInput = popupElement.querySelector('.popup__input_type_job');
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
